fc392d9c0b08b8f6fcbf11b7597896420a31b2a4
Daniel Rodriguez <drod2793@gmail.com>
2017-08-11 18:16:15 -0600

    Electronic Life: fixes spacing

--- Projects/Electronic Life/grid.js
+++ Projects/Electronic Life/grid.js
@@ -1,22 +1,23 @@
 function Grid(width, height) {
-    this.width = width;
-    this.height = height;
-    this.space = new Array();
-    for(let i = 0; i < height; i++) {
-        this.space[i] = new Array(width);
-    }
+  this.width = width;
+  this.height = height;
+  this.space = new Array();
+  for(let i = 0; i < height; i++) {
+    this.space[i] = new Array(width);
+  }
 }
+
 Grid.prototype.isInside = function(vector) {
   return vector.x >= 0 && vector.x < this.width &&
          vector.y >= 0 && vector.y < this.height;
 };
 
 Grid.prototype.get = function(vector) {
-    return this.space[vector.x][vector.y];
+  return this.space[vector.x][vector.y];
 }
 
 Grid.prototype.set = function(vector, value) {
-    this.space[vector.x][vector.y] = value;
+  this.space[vector.x][vector.y] = value;
 }
 
 module.exports = Grid;
