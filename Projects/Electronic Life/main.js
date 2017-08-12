544c72eef956218b6b7808765c21d77453919256
Daniel Rodriguez <drod2793@gmail.com>
2017-08-11 19:33:45 -0600

    Electronic Life:add main.js

---
+++ Projects/Electronic Life/main.js
@@ -0,0 +1,32 @@
+const Vector = require("./vector.js");
+const Grid = require("./grid.js");
+const World = require("./world.js");
+const Wall = require("./wall.js");
+const BouncingCritter = require("./bouncing_critter.js");
+
+const plan = [
+  ["############################"],
+  ["#      #    #      o      ##"],
+  ["#                          #"],
+  ["#          #####           #"],
+  ["##         #   #    ##     #"],
+  ["###           ##     #     #"],
+  ["#           ###      #     #"],
+  ["#   ####                   #"],
+  ["#   ##       o             #"],
+  ["# o  #         o       ### #"],
+  ["#    #                     #"],
+  ["############################"]
+];
+const legend = {"#": Wall, "o": BouncingCritter};
+
+const grid = new Grid(5, 5);
+console.log(grid.get(new Vector(1, 1)));
+// → undefined
+grid.set(new Vector(1, 1), "X");
+console.log(grid.get(new Vector(1, 1)));
+// → X
+
+//Getting new legend[ch] is not a constructor when working across files
+var world = new World(plan, legend);
+console.log(world.toString());
