fb40113c2341c1b704d9d2e2c8127c83dd421e9c
Daniel Rodriguez <drod2793@gmail.com>
2017-08-14 18:43:29 -0600

    Electronic Life: adds turn, letAct, and checkDestination functions to world

--- Projects/Electronic Life/world.js
+++ Projects/Electronic Life/world.js
@@ -41,4 +41,34 @@ World.prototype.toString = function() {
   return output;
 }
 
+World.prototype.turn = function() {
+  var acted = [];
+  this.grid.forEach(function(critter, vector) {
+  if (critter.act && acted.indexOf(critter) == -1) {
+    acted.push(critter);
+    this.letAct(critter, vector);
+    }
+  }, this)
+}
+
+World.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this.vector));
+  if (action && action.type == "move") {
+    var dest = this.checkDestination(action, vector);
+    //moves critter to new location if valid and empty
+    if(dest && this.grid.get(dest) == null) {
+      this.grid.set(vector, null);
+      this.grid.set(dest, critter);
+    }
+  }
+}
+
+World.prototype.checkDestination = function(action, vector) {
+  if (directions.hasOwnProperty(action.direction)) {
+    var dest = vector.plus(directions[action.direction]);
+    if (this.grid.isInside(dest))
+      return dest;
+  }
+};
+
 module.exports = World;
