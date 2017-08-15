635076e0ae35eb8ac38523a714ab8439fcd9e40b
Daniel Rodriguez <drod2793@gmail.com>
2017-08-14 20:12:11 -0600

    Electronic Life: adds wallfollower.js

--- Projects/Electronic Life/main.js
+++ Projects/Electronic Life/main.js
@@ -3,22 +3,18 @@ const Grid = require("./grid.js");
 const World = require("./world.js");
 const Wall = require("./wall.js");
 const BouncingCritter = require("./bouncing_critter.js");
+const WallFollower = require("./wallfollower.js");
 
 const plan = [
-  "############################",
-  "#      #    #      o      ##",
-  "#                          #",
-  "#          #####           #",
-  "##         #   #    ##     #",
-  "###           ##     #     #",
-  "#           ###      #     #",
-  "#   ####                   #",
-  "#   ##       o             #",
-  "# o  #         o       ### #",
-  "#    #                     #",
-  "############################"
+  "############",
+  "#     #    #",
+  "#   ~    ~ #",
+  "#  ##      #",
+  "#  ##  o####",
+  "#          #",
+  "############"
 ];
-const legend = {"#": Wall, "o": BouncingCritter};
+const legend = {"#": Wall, "~": WallFollower, "o": BouncingCritter};
 
 const grid = new Grid(5, 5);
 console.log(grid.get(new Vector(1, 1)));
