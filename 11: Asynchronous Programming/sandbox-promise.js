234bff36954ea5e6504894e9a6f532a8dd67df50
Daniel Rodriguez <drod2793@gmail.com>
2018-04-05 19:39:19 -0600

    11: Adds Request type given handler and promise.all

--- 11: Asynchronous Programming/sandbox-promise.js
+++ 11: Asynchronous Programming/sandbox-promise.js
@@ -7,8 +7,8 @@ function storage(nest, name) {
   });
 }
 
-storage(bigOak, "enemies")
-  .then(value => console.log("Got", value));
+// storage(bigOak, "enemies")
+//   .then(value => console.log("Got", value));
 
 class Timeout extends Error {}
 
@@ -31,5 +31,31 @@ function request(nest, target, type, content) {
   });
 }
 
-request(bigOak, "Cow Pasture", "note", "Let's caw loudly at 7PM")
-.then(console.log("Note delivered."))
+function requestType(name, handler) {
+  defineRequestType(name, (nest, content, source,
+                           callback) => {
+    try {
+      Promise.resolve(handler(nest, content, source))
+        .then(response => callback(null, response),
+              failure => callback(failure));
+    } catch (exception) {
+      callback(exception);
+    }
+  });
+}
+
+requestType("ping", () => "pong");
+
+function availableNeighbors(nest) {
+  let requests = nest.neighbors.map(neighbor => {
+    return request(nest, neighbor, "ping")
+      .then(() => true, () => false);
+  });
+  return Promise.all(requests).then(result => {
+    return nest.neighbors.filter((_, i) => result[i]);
+  });
+}
+
+availableNeighbors(bigOak).then(response => {
+  console.log(response);
+})
